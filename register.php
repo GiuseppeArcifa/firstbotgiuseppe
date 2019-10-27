9f1d9b5bec1d9a499f7e352ade26a0cf53619e66
GiuseppeArcifa <52871665+GiuseppeArcifa@users.noreply.github.com>
2019-10-27 12:38:24 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://firstbotgiuseppe.herokuapp.com/';
+$WEBHOOK_URL = 'https://firstbotgiuseppe.herokuapp.com/index.php';
 $BOT_TOKEN = '997280523:AAGScn7XbNbKaeFu9Jgo1bh9f-8HvVuyZm0';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
